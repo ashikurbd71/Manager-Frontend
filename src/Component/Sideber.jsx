55eed27a17929ad59af16ee784f2830365a90e08
ashikurovibd71 <ashikurovi2003@gmail.com>
2024-07-31 13:50:30 +0600

    update ui

--- src/Component/Sideber.jsx
+++ src/Component/Sideber.jsx
@@ -2,44 +2,42 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaMagnet, FaMoneyBillTransfer, FaUserPlus } from "react-icons/fa6";
-import { MdArrowDropDown, MdArrowRight, MdOutlineDashboard ,MdOutlineSettings} from "react-icons/md";
-import img from "../assets/manager.png"
+import { MdArrowDropDown, MdArrowRight, MdOutlineDashboard, MdOutlineSettings } from "react-icons/md";
+import img from "../assets/manager.png";
 import { LuCircleDot } from "react-icons/lu";
 import { FaUserGear } from "react-icons/fa6";
 import { TbNews } from "react-icons/tb";
 import { FaAward } from "react-icons/fa";
 const Sideber = () => {
-
-    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
   const toggleSubMenu = () => {
     setIsSubMenuOpen(!isSubMenuOpen);
   };
 
-
   return (
     <>
       {" "}
       <div className="flex flex-col gap-3 pt-4 h-full bg-[#CCD3CA] items-center">
-      <div className="flex justify-items-center mb-5 items-center">
-        <img src={img} alt="" className="w-8 h-8"/>
-        <h1 className="text-2xl font-bold text-black "> <span className="text-[#0284C7]">Man</span>ager</h1>
-       </div>
-       
-       <NavLink
+        <div className="flex justify-items-center mb-5 items-center">
+          <img src={img} alt="" className="w-8 h-8" />
+          <h1 className="text-2xl font-bold text-black ">
+            {" "}
+            <span className="text-[#0284C7]">Man</span>ager
+          </h1>
+        </div>
+
+        <NavLink
           to="/"
           className={({ isActive, isPending }) =>
             isPending ? "pending" : isActive ? " bg-[#0284C7] px-10 pt-1 rounded-md text-white " : ""
           }
         >
-            <div className="flex -ml-3 justify-center pb-2 hover:text-gray-600 items-center gap-1">
-            <MdOutlineDashboard className="text-lg"/>
-             <h1 className="text-lg font-medium">Dashboard</h1>
-            </div>
-        
+          <div className="flex -ml-3 justify-center pb-2 hover:text-gray-600 items-center gap-1">
+            <MdOutlineDashboard className="text-lg" />
+            <h1 className="text-lg font-medium">Dashboard</h1>
+          </div>
         </NavLink>
-
-
 
         <NavLink
           to="/dashboard/memberlist"
@@ -47,28 +45,24 @@ const Sideber = () => {
             isPending ? "pending" : isActive ? "bg-[#0284C7] px-6 pt-1 rounded-md text-white " : ""
           }
         >
-            <div className="flex justify-center pb-2 hover:text-gray-600 items-center gap-1">
-            <FaUserPlus className="text-lg"/>
-             <h1 className="text-lg font-medium"> Add Member</h1>
-            </div>
-        
+          <div className="flex justify-center pb-2 hover:text-gray-600 items-center gap-1">
+            <FaUserPlus className="text-lg" />
+            <h1 className="text-lg font-medium"> Add Member</h1>
+          </div>
         </NavLink>
 
         {/* manager */}
-      
 
-        
         <NavLink
           to="/dashboard/managerlist"
           className={({ isActive, isPending }) =>
             isPending ? "pending" : isActive ? "bg-[#0284C7] px-6 pt-1 rounded-md text-white " : ""
           }
         >
-            <div className="flex justify-center pb-2 hover:text-gray-600 items-center gap-1">
-            <FaUserGear className="text-lg"/>
-             <h1 className="text-lg font-medium"> Add Manager</h1>
-            </div>
-        
+          <div className="flex justify-center pb-2 hover:text-gray-600 items-center gap-1">
+            <FaUserGear className="text-lg" />
+            <h1 className="text-lg font-medium"> Add Manager</h1>
+          </div>
         </NavLink>
 
         <NavLink
@@ -77,110 +71,93 @@ const Sideber = () => {
             isPending ? "pending" : isActive ? "bg-[#0284C7] px-10 pt-1 rounded-md text-white " : ""
           }
         >
-            <div className="flex justify-center -ml-6 pb-2 hover:text-gray-600 items-center gap-1">
-            <TbNews className="text-lg"/>
-             <h1 className="text-lg font-medium"> Add Notice</h1>
-            </div>
-        
+          <div className="flex justify-center -ml-6 pb-2 hover:text-gray-600 items-center gap-1">
+            <TbNews className="text-lg" />
+            <h1 className="text-lg font-medium"> Add Notice</h1>
+          </div>
         </NavLink>
-
 
         <NavLink
           to="/dashboard/mealmanagelist"
           className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "bg-[#0284C7] px-8 pt-2 rounded-md text-white " : ""
+            isPending ? "pending" : isActive ? "bg-[#0284C7] px-6 pt-2 rounded-md text-white " : ""
           }
         >
-            <div className="flex justify-center -ml-6 pb-2 hover:text-gray-600 items-center gap-1">
-            <FaAward className="text-lg"/>
-             <h1 className="text-lg font-medium">Meal Manage</h1>
-            </div>
-        
+          <div className="flex justify-center  pb-2 hover:text-gray-600 items-center gap-1">
+            <FaAward className="text-lg" />
+            <h1 className="text-lg font-medium">Meal Manage</h1>
+          </div>
         </NavLink>
-
-
 
         <NavLink
           to="/dashboard/monthlyreport"
           className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "bg-[#0284C7] px-8 pt-2 rounded-md text-white " : ""
+            isPending ? "pending" : isActive ? "bg-[#0284C7] px-6 pt-2 rounded-md text-white " : ""
           }
         >
-            <div className="flex justify-center -ml-6 pb-2 hover:text-gray-600 items-center gap-1">
-            <FaMoneyBillTransfer className="text-lg"/>
-             <h1 className="text-lg font-medium">Monthly Report</h1>
-            </div>
-        
+          <div className="flex justify-center pb-2 ml-4 hover:text-gray-600 items-center gap-1">
+            <FaMoneyBillTransfer className="text-lg" />
+            <h1 className="text-lg font-medium">Monthly Report</h1>
+          </div>
         </NavLink>
-
-
-    
-
 
         {/* setting */}
 
-        
         <div>
-      <button
-   
-    
-      >
-        <div
-          className="flex justify-items-center pb-2 -ml-10 hover:text-gray-600 items-center gap-1"
-          onClick={toggleSubMenu}
-        >
-          <MdOutlineSettings className="text-lg"/>
-          <h1 className="text-lg font-medium">Setting</h1>
-          {isSubMenuOpen ? <MdArrowDropDown className="text-lg"/> : <MdArrowRight className="text-lg"/>}
+          <button>
+            <div
+              className="flex justify-items-center pb-2 -ml-10 hover:text-gray-600 items-center gap-1"
+              onClick={toggleSubMenu}
+            >
+              <MdOutlineSettings className="text-lg" />
+              <h1 className="text-lg font-medium">Setting</h1>
+              {isSubMenuOpen ? <MdArrowDropDown className="text-lg" /> : <MdArrowRight className="text-lg" />}
+            </div>
+          </button>
+          {isSubMenuOpen && (
+            <div className="pt-2">
+              <NavLink
+                to="/dashboard/setting/institutelist"
+                className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
+              >
+                <div className="flex items-center  hover:text-gray-600 gap-1 ">
+                  <LuCircleDot className="text-lg" />
+                  <h2 className="text-md font-medium">Institute</h2>
+                </div>
+              </NavLink>
+
+              <NavLink
+                to="/dashboard/setting/departmentlist"
+                className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
+              >
+                <div className="flex items-center  hover:text-gray-600 gap-1 py-2 ">
+                  <LuCircleDot className="text-lg" />
+                  <h2 className="text-md font-medium">Departmnet</h2>
+                </div>
+              </NavLink>
+
+              <NavLink
+                to="/dashboard/setting/semisterlist"
+                className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
+              >
+                <div className="flex items-center  hover:text-gray-600 gap-1 pb-2 ">
+                  <LuCircleDot className="text-lg" />
+                  <h2 className="text-md font-medium">Semister</h2>
+                </div>
+              </NavLink>
+
+              <NavLink
+                to="/dashboard/setting/bloodgrouplist"
+                className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
+              >
+                <div className="flex items-center  hover:text-gray-600 gap-1 ">
+                  <LuCircleDot className="text-lg" />
+                  <h2 className="text-md font-medium">Blood Group</h2>
+                </div>
+              </NavLink>
+            </div>
+          )}
         </div>
-      </button>
-      {isSubMenuOpen && (
-        <div className="pt-2">
-
-<NavLink
-            to="/dashboard/setting/institutelist"
-            className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
-          >
-            <div className="flex items-center  hover:text-gray-600 gap-1 ">
-            <LuCircleDot className="text-lg"/>
-              <h2 className="text-md font-medium">Institute</h2>
-            </div>
-          </NavLink>
-
-          <NavLink
-            to="/dashboard/setting/departmentlist"
-            className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
-          >
-            <div className="flex items-center  hover:text-gray-600 gap-1 py-2 ">
-              <LuCircleDot className="text-lg"/>
-              <h2 className="text-md font-medium">Departmnet</h2>
-            </div>
-          </NavLink>
-        
-
-          
-          <NavLink
-            to="/dashboard/setting/semisterlist"
-            className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
-          >
-            <div className="flex items-center  hover:text-gray-600 gap-1 pb-2 ">
-            <LuCircleDot className="text-lg"/>
-              <h2 className="text-md font-medium">Semister</h2>
-            </div>
-          </NavLink>
-
-          <NavLink
-            to="/dashboard/setting/bloodgrouplist"
-            className={({ isActive }) => (isActive ? "text-[#0284C7] " : "")}
-          >
-            <div className="flex items-center  hover:text-gray-600 gap-1 ">
-            <LuCircleDot className="text-lg"/>
-              <h2 className="text-md font-medium">Blood Group</h2>
-            </div>
-          </NavLink>
-        </div>
-      )}
-    </div>
       </div>
     </>
   );
--- src/DashPages/Details/Manager/Meal/Mealdetails.jsx
+++ src/DashPages/Details/Manager/Meal/Mealdetails.jsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
-import { FaPrint, FaUser } from 'react-icons/fa6';
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
+import { FaPrint, FaUser } from "react-icons/fa6";
 
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import axoissecure from '../../../../Hooks/Axoisscure';
-import DashCustomNav from '../../../../Share/Formnav';
-import { MdContactEmergency } from 'react-icons/md';
-import MealEmergency from '../../../Update/MealManager/MealEmergency/MealEmergency';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axoissecure from "../../../../Hooks/Axoisscure";
+import DashCustomNav from "../../../../Share/Formnav";
+import { MdContactEmergency } from "react-icons/md";
+import MealEmergency from "../../../Update/MealManager/MealEmergency/MealEmergency";
 
 const Mealdetails = () => {
-
-
- const[member,setMember] = useState()
+  const [member, setMember] = useState();
   const { id } = useParams();
 
   const { data, refetch } = useQuery({
@@ -30,11 +28,11 @@ const Mealdetails = () => {
     },
   });
 
-  console.log(member)
+  console.log(member);
 
   const join = member?.date;
   const parsedDate = new Date(join);
-  
+
   // Format the date as "14 May 2024"
   const formattedDate = parsedDate.toLocaleDateString("en-US", {
     year: "numeric",
@@ -42,8 +40,7 @@ const Mealdetails = () => {
     day: "2-digit",
   });
 
-
-  const { data: totalitem = [],  } = useQuery({
+  const { data: totalitem = [] } = useQuery({
     queryKey: ["extrag"],
     queryFn: async () => {
       try {
@@ -56,8 +53,7 @@ const Mealdetails = () => {
     },
   });
 
-
-  const { data: extra = [],  } = useQuery({
+  const { data: extra = [] } = useQuery({
     queryKey: ["extra"],
     queryFn: async () => {
       try {
@@ -70,142 +66,106 @@ const Mealdetails = () => {
     },
   });
 
-  console.log(extra)
+  console.log(extra);
 
- console.log(totalitem)
+  console.log(totalitem);
 
+  const extramoney = (extra / totalitem).toFixed(2);
 
-
-
-  const extramoney = (extra / totalitem).toFixed(2)
-
-
-  const [isOpen, setIsOpen] = useState(null)
-  const[update,setUpdate] = useState()
-
+  const [isOpen, setIsOpen] = useState(null);
+  const [update, setUpdate] = useState();
 
   const openModal = (id) => {
-    setIsOpen(true)
-    setUpdate(id)
-  }
+    setIsOpen(true);
+    setUpdate(id);
+  };
 
-
-    return (
-      <>
+  return (
+    <>
       <Helmet>
         <title>Manager || Meal details</title>
       </Helmet>
       <MealEmergency isOpen={isOpen} setIsOpen={setIsOpen} update={update} refetch={refetch} />
-      <DashCustomNav name={'Meal details'} listroute={'/dashboard/mealmanagelist'}/>
-     
-        <div className='px-10 pb-10'>
-     
+      <DashCustomNav name={"Meal details"} listroute={"/dashboard/mealmanagelist"} />
 
-          <div className='flex  justify-end gap-5 pb-5'>
-
-          <div onClick={ () => openModal(member)} className='flex border-2 bg-red-500  cursor-pointer item items-center px-3 py-1 gap-1'>
-         
-            <h1 className='text-lg font-medium text-white'>Emergency</h1>
-            </div>
-
-            <div className='flex border-2 cursor-pointer item items-center px-3 py-1 gap-1'>
-            <FaPrint className='text-bl text-blue-400'/>
-            <h1 className='text-lg font-medium text-gray-500'> Print</h1>
-            </div>
-
+      <div className="px-10 pb-10">
+        <div className="flex  justify-end gap-5 pb-5">
+          <div
+            onClick={() => openModal(member)}
+            className="flex border-2 bg-red-500  cursor-pointer item items-center px-3 py-1 gap-1"
+          >
+            <h1 className="text-lg font-medium text-white">Emergency</h1>
           </div>
 
-          <div className=' bg-white min min-h-screen'>
-
-  <div className='  items-center p-10 flex'>
-
-  </div>
-           
-      <div className='flex flex-col mx-10 gap-1 '>
-
- 
-    <h1 className='text-left text-[24px]  font-semibold  text-[#0284C7]'>{member?.member?.name}</h1>
-    
-      <h1 className='text-gray-500  -mt-2'>Ranpur Ideal Institute Technology</h1>
-      <h1 className='text-gray-500  -mt-2'>Computer scince And Technology(4th)</h1>
-      </div>
-
-      
-      <div className="text-gray-700 p-10">
-  <div className="overflow-x-auto">
-    
-    <table className="w-full border border-gray-200 text-sm">
-        
-      <tbody>
-    
-
-  
-
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Total Money</td>
-          <td className="px-4 py-2 border border-gray-200">{member?.addMoney}Tk</td>
-        </tr>
-
-        
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Total Meal</td>
-          <td className="px-4 py-2 border border-gray-200">{member?.totalMeal}</td>
-        </tr>
-
-        
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Blance</td>
-          <td className="px-4 py-2 border border-gray-200">{member?.blance}Tk</td>
-        </tr>
-
-        
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Meal Eat</td>
-          <td className="px-4 py-2 border border-gray-200">{member?.eatMeal}</td>
-        </tr>
-
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Loan</td>
-          <td className="px-4 py-2 border border-gray-200">{member?.loan || 0}Tk</td>
-        </tr>
-
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Guest Meal</td>
-          <td className="px-4 py-2 border border-gray-200">{member?.guest || 0}</td>
-        </tr>
-
-
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Total Extra</td>
-          <td className="px-4 py-2 border border-gray-200">{extramoney || 'Loading...'}Tk</td>
-        </tr>
-
-      
-    
-        <tr>
-          <td className="px-4 py-2 font-semibold border border-gray-200">Last Payment</td>
-          <td className="px-4 py-2 border border-gray-200">{formattedDate}</td>
-        </tr>
-     
-      
-      </tbody>
-    </table>
-  </div>
-
-    
-
-</div>
-
-
-
-
-
+          <div className="flex border-2 cursor-pointer item items-center px-3 py-1 gap-1">
+            <FaPrint className="text-bl text-blue-400" />
+            <h1 className="text-lg font-medium text-gray-500"> Print</h1>
           </div>
-            
         </div>
 
-        </>
-    );
+        <div className=" bg-white min min-h-screen">
+          <div className="  items-center p-10 flex"></div>
+
+          <div className="flex flex-col mx-10 gap-1 ">
+            <h1 className="text-left text-[24px]  font-semibold  text-[#0284C7]">{member?.member?.name}</h1>
+
+            <h1 className="text-gray-500  -mt-2">{member?.member?.instituteName?.name}</h1>
+            <h1 className="text-gray-500  -mt-2">
+              {member?.member?.department?.name}({member?.member?.semister?.shortName})
+            </h1>
+          </div>
+
+          <div className="text-gray-700 p-10">
+            <div className="overflow-x-auto">
+              <table className="w-full border border-gray-200 text-sm">
+                <tbody>
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Total Money</td>
+                    <td className="px-4 py-2 border border-gray-200">{member?.addMoney}Tk</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Total Meal</td>
+                    <td className="px-4 py-2 border border-gray-200">{member?.totalMeal}</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Blance</td>
+                    <td className="px-4 py-2 border border-gray-200">{member?.blance}Tk</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Meal Eat</td>
+                    <td className="px-4 py-2 border border-gray-200">{member?.eatMeal}</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Loan</td>
+                    <td className="px-4 py-2 border border-gray-200">{member?.loan || 0}Tk</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Guest Meal</td>
+                    <td className="px-4 py-2 border border-gray-200">{member?.guest || 0}</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Total Extra</td>
+                    <td className="px-4 py-2 border border-gray-200">{extramoney || "Loading..."}Tk</td>
+                  </tr>
+
+                  <tr>
+                    <td className="px-4 py-2 font-semibold border border-gray-200">Last Payment</td>
+                    <td className="px-4 py-2 border border-gray-200">{formattedDate}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Mealdetails;
